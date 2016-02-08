bffe29a9d6ccd97b328a080c41c767f844918260
nuraalam <aklima.alam13@gmail.com>
2016-02-08 15:27:13 +0600

    gfhgfhfhg

---
+++ HTMLPageDesign/HTMLPageDesign/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HTMLPageDesign.Startup))]
+namespace HTMLPageDesign
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
